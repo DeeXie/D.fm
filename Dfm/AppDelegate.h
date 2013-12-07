2c5355950edc621f4f7101bc48cd36edd85bda16
dee <deexie1990@gmail.com>
2013-12-07 10:09:31 +0800

    Initial Commit

---
+++ Dfm/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dfm
+//
+//  Created by xieweizhi on 12/7/13.
+//  Copyright (c) 2013 xieweizhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dfm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dfm
+//
+//  Created by xieweizhi on 12/7/13.
+//  Copyright (c) 2013 xieweizhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
